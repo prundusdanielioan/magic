e75f419cce181df712768f08ba847dded5b6611b
Prundus Daniel <prundusdanielioan@gmail.com>
2024-05-12 03:52:04 +0300

    image light box

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,27 @@
 <livewire:rezervari/>
 @livewireScripts
 <div class="container">
-
-    <h1>My First Bootstrap Page</h1>
-    <p>This is some text.</p>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/css/lightbox.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/js/lightbox.min.js"></script>
+    <div class="lightbox-gallery">
+        <div class="container">
+            <div class="intro">
+                <h2 class="text-center">Lightbox Gallery</h2>
+                <p class="text-center">Find the lightbox gallery for your project. click on any image to open
+                    gallary</p>
+            </div>
+            <div class="row photos">
+                <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="{{ asset('storage/images/reclama1.jpg')}}"
+                                                                data-lightbox="photos"><img class="img-fluid"
+                                                                                            src="{{ asset('storage/images/reclama1.jpg')}}"></a>
+                </div>
+                <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="{{ asset('storage/images/reclama2.jpg')}}"
+                                                                data-lightbox="photos"><img class="img-fluid"
+                                                                                            src="{{ asset('storage/images/reclama2.jpg')}}"></a>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @include('footer')
 

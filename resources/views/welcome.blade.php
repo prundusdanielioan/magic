d62033388ef1a8aabca77cf76fdc3132cbc88d5a
Prundus Daniel <prundusdanielioan@gmail.com>
2024-05-12 18:07:39 +0300

    bg image and nav bar color change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,9 @@
 @include('base')
-<livewire:counter/>
-@livewireScripts
-<livewire:rezervari/>
-@livewireScripts
 <div class="container">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/css/lightbox.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/js/lightbox.min.js"></script>
     <div class="lightbox-gallery">
         <div class="container">
-            <div class="intro">
-                <h2 class="text-center">Lightbox Gallery</h2>
-                <p class="text-center">Find the lightbox gallery for your project. click on any image to open
-                    gallary</p>
-            </div>
             <div class="row photos">
                 <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="{{ asset('storage/images/reclama1.jpg')}}"
                                                                 data-lightbox="photos"><img class="img-fluid"

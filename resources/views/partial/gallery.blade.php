b5e4f1c52d0b4ee01387d243741a2727f84aba76
Prundus Daniel <prundusdanielioan@gmail.com>
2024-05-22 20:46:46 +0300

    use s3 for images

--- resources/views/partial/gallery.blade.php
+++ resources/views/partial/gallery.blade.php
@@ -4,13 +4,13 @@
 <div class="lightbox-gallery">
     <div class="container">
         <div class="row photos">
-            <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="{{ asset('storage/images/reclama1.jpg')}}"
+            <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="{{ Storage::disk('s3')->url('images/reclama1.jpg') }}"
                                                             data-lightbox="photos"><img class="img-fluid"
-                                                                                        src="{{ asset('storage/images/reclama1.jpg')}}"></a>
+                                                                                        src="{{ Storage::disk('s3')->url('images/reclama1.jpg') }}"></a>
             </div>
-            <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="{{ asset('storage/images/reclama2.jpg')}}"
+            <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="{{ Storage::disk('s3')->url('images/reclama1.jpg') }}"
                                                             data-lightbox="photos"><img class="img-fluid"
-                                                                                        src="{{ asset('storage/images/reclama2.jpg')}}"></a>
+                                                                                        src="{{ Storage::disk('s3')->url('images/reclama1.jpg') }}"></a>
             </div>
         </div>
     </div>

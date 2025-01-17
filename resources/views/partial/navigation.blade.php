538ae32b9dcb1f20dadf70c426286100086df39a
Prundus Daniel <prundusdanielioan@gmail.com>
2025-01-17 22:24:39 +0200

    final

--- resources/views/partial/navigation.blade.php
+++ resources/views/partial/navigation.blade.php
@@ -5,21 +5,17 @@
 </div>
 <nav class="navbar navbar-icon-top navbar-expand-lg">
 
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
+        <ul class="navbar-nav ms-auto">
             <li class="nav-item active me-4">
                 <a class="nav-link" href="/">
-                    <i class="fa fa-home "></i>
+                    <i class="fa fa-home"></i>
                     Acasa
                     <span class="sr-only">(current)</span>
-                </a>
-            </li>
-            <li class="nav-item me-4">
-                <a class="nav-link" href="{{ route('rezervari') }}">
-                    <i class="fa fa-envelope-o">
-                        <span class="badge badge-danger"></span>
-                    </i>
-                    Rezervari
                 </a>
             </li>
 
@@ -31,6 +27,7 @@
                     Galerie
                 </a>
             </li>
+
             @auth
                 <li class="nav-item me-4">
                     <a class="nav-link" href="{{ route('rezervari') }}">
@@ -40,16 +37,8 @@
                         Rezervari
                     </a>
                 </li>
-
-                <li class="nav-item me-4">
-                    <a class="nav-link" href="{{ route('gallery') }}">
-                        <i class="fa fa-file-image-o">
-                            <span class="badge badge-danger"></span>
-                        </i>
-                        Galerie
-                    </a>
-                </li>
             @endauth
+
             @if (Auth::check())
                 <li class="nav-item me-4">
                     <a class="nav-link" href="{{ route('list') }}">
@@ -65,19 +54,15 @@
                         <i class="fa fa-sign-out"></i>
                         Logout
                     </a>
-
                 </li>
             @else
-
                 <li class="nav-item me-4">
                     <a class="nav-link" href="{{ route('login') }}">
                         <i class="fa fa-sign-in"></i>
-                        My Login
+                        Login
                     </a>
                 </li>
             @endif
-
-
         </ul>
     </div>
 </nav>
